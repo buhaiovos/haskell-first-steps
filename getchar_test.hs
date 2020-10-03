5e8a6793ae653e768ab095f100774ca7fdac5608
buhaiovos <oleksandr.buhaiov@gmail.com>
2020-10-03 20:19:39 +0300

    replace if expression with Control.Monad.when

--- getchar_test.hs
+++ getchar_test.hs
@@ -1,7 +1,7 @@
+import Control.Monad
+
 main = do
     c <- getChar
-    if c /= ' '
-        then do
-            putChar c
-            main
-        else return ()
+    when (c /= ' ') $ do
+      putChar c
+      main
