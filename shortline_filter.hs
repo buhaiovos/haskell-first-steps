490cf67085fb87d85fe2a2088524bd9e7d1638b0
buhaiovos <oleksandr.buhaiov@gmail.com>
2020-10-03 21:16:37 +0300

    go composition-crazy with shortline

--- shortline_filter.hs
+++ shortline_filter.hs
@@ -1,8 +1,1 @@
-main = interact onlyShortLines
-
-onlyShortLines :: String -> String
-onlyShortLines text =
-  let allLines = lines text
-      shortLines = filter (\l -> length l < 10) allLines
-      result = unlines shortLines
-  in result
+main = interact $ unlines . filter ((<10) . length) . lines
