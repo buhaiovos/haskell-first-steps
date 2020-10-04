da77bb7e418c7ac6f375e951c17ad5c23a1953ba
buhaiovos <oleksandr.buhaiov@gmail.com>
2020-10-04 13:07:11 +0300

    read file using readFile function

--- inputoutput/girlfriend.hs
+++ inputoutput/girlfriend.hs
@@ -1,13 +1,5 @@
 import System.IO
 
 main = do
-  withFile' "girlfriend.txt" ReadMode (\handle -> do
-    contents <- hGetContents handle
-    putStr contents)
-
-withFile' :: FilePath -> IOMode -> (Handle -> IO a) -> IO a
-withFile' path mode f = do
-  handle <- openFile path mode
-  result <- f handle
-  hClose handle
-  return result
+  contents <- readFile "girlfriend.txt"
+  putStr contents
