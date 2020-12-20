b9321ad32168a7c6ec3e81e8ccf7b7f22a7818f4
buhaiovos <oleksandr.buhaiov@gmail.com>
2020-12-20 16:03:09 +0200

    How functions are monads (Readers)

---
+++ moremonads/function-as-monad-do.hs
@@ -0,0 +1,18 @@
+import           Control.Monad.Instances
+
+addStuff :: Int -> Int
+addStuff = do
+  a <- (*2)
+  b <- (+10)
+  return (a + b)
+
+addStuff' :: Int -> Int
+addStuff' = (*2)  >>= (\a ->
+            (+10) >>= (\b ->
+            return (a + b)))
+
+addStuff'' :: Int -> Int
+addStuff'' x = let
+  a = (*2) x
+  b = (+10) x
+  in a + b
